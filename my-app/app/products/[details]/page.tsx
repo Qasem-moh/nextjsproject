1611dd9c0fc076dcda0a913398266d5cc4af029f
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-14 17:08:26 +0300

    next

--- my-app/app/products/[details]/page.tsx
+++ my-app/app/products/[details]/page.tsx
@@ -1,3 +1,22 @@
+import {Metadata} from "next";
+
+type Props = {
+    params: {
+        productid: string
+    }
+}
+export const generateMetadata = async ({params}: Props):Promis<Metadata> => {
+    const title=await new Promise((resolve=>{
+        setTimeout(()=>{
+            resolve(`iphone ${params.details}`)
+        },1000)
+    }))
+    return {
+        title: `Product ${title}`
+    }
+}
+
+
 export default function Details({params}: { params: { details: string } }) {
     return (<div>
             <h1>Hello World!</h1>
